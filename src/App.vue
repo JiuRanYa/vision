b00e10879668dbe30b46cb682bf34ea5165a8731
JiuRanYa <1084350607@qq.com>
2025-09-18 10:27:38 +0800

    feat: add button components and integrate KTUI styles into the application

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,20 @@
   <div class="text-3xl font-bold underline">
     123
   </div>
+
+  <div class="flex flex-wrap gap-4">
+    <button type="button" class="kt-btn">
+      Primary
+    </button><button type="button" class="kt-btn kt-btn-secondary">
+      Secondary
+    </button><button type="button" class="kt-btn kt-btn-destructive">
+      Destructive
+    </button><button type="button" class="kt-btn kt-btn-mono">
+      Mono
+    </button><button type="button" class="kt-btn kt-btn-outline">
+      Outline
+    </button><button type="button" class="kt-btn kt-btn-ghost">
+      Ghost
+    </button>
+  </div>
 </template>
