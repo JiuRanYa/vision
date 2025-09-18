549f9a93ab38ebff20357d5fbf4509927c09389d
JiuRanYa <1084350607@qq.com>
2025-09-18 10:52:34 +0800

    feat: add sidebar component and update layout structure in index.vue

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import Sidebar from './sidebar.vue'
+</script>
+
 <template>
-  <div>123</div>
+  <div class="flex">
+    <Sidebar />
+
+    <div class="bg-surface-0 flex flex-1 flex-col overflow-hidden md:h-screen" />
+  </div>
 </template>
---
+++ src/layout/sidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="z-[100] h-dvh overflow-hidden lg:static relative transition-all duration-100 w-16 xl:w-60">
+    <div class="text-sm bg-gray-100 h-full">
+      123
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
